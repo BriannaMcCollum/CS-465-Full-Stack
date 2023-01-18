072c480af55e8504f53b475250a7909d5c4b9116
BriannaMcCollum <brianna.mccollum@snhu.edu>
2023-01-17 20:10:31 -0600

    Added Data folder and JSON data templating

--- travlr/NewTravlr/app_server/controllers/travel.js
+++ travlr/NewTravlr/app_server/controllers/travel.js
@@ -1,8 +1,12 @@
+/* Pulls what's needed to read the JSON file then reads it's data */
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: 'Travlr Getaways', trips });
 };
+
 module.exports = {
     travel
 };
-   
