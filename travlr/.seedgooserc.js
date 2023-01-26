34ed25adf4808e715d00a39d43d8394c17da320a
BriannaMcCollum <brianna.mccollum@snhu.edu>
2023-01-25 19:34:21 -0600

    New NoSQL Database to hold trips.json data

---
+++ travlr/.seedgooserc.js
@@ -0,0 +1,7 @@
+// .seedgooserc.js
+module.exports = {
+    modelBaseDirectory: "app_server/database/models", // model directory name
+    models: ["*.js", "!db.js"], // model matches
+    data: "data", // data directory
+    db: "mongodb://127.0.0.1:27017/travlr" // db connection url
+    };
