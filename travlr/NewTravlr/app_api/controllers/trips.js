e2ae5a4123736a5455b688b19f7819f4996d914c
BriannaMcCollum <brianna.mccollum@snhu.edu>
2023-02-01 16:34:08 -0600

    REST API endpoints for Separation of Concerns

---
+++ travlr/NewTravlr/app_api/controllers/trips.js
@@ -0,0 +1,53 @@
+const mongoose =  require('mongoose'); //.set('debug', true);
+const model = mongoose.model('trips');
+
+// GET: /trips = lists all the tips
+const tripsList = async (req, res) => {
+    model
+        .find({}) // empty filter for all
+        .exec((err, trips) => {
+            if (!trips) {
+                return res
+                    .status(404)
+                    .json({"message":"trips not found"});
+            }
+            else if (err) {
+                return res
+                    .status(404)
+                    .json(err);
+            }
+            else {
+                return res
+                    .status(200)
+                    .json(trips);
+            }
+        });
+};
+
+// GET /trips/:tripCode - returns a single trip
+const tripsFindCode = async (req, res) => {
+    model
+        .find({ 'code': req.params.tripCode })
+        .exec((err, trip) => {
+            if (!trip) {
+                return res
+                    .status(404)
+                    .json({ "message": "trip not found" });
+            }
+            else if (err) {
+                return res
+                    .status(404)
+                    .json(err);
+            }
+            else {
+                return res
+                    .status(200)
+                    .json(trip);
+            }
+        });
+};
+
+module.exports = {
+    tripsList,
+    tripsFindCode
+};
---
+++ travlr/NewTravlr/app_api/database/db.js
@@ -0,0 +1,57 @@
+var mongoose = require('mongoose');
+var gracefulShutdown;
+var dbURI = 'mongodb://127.0.0.1:27017/travlr';
+if (process.env.NODE_ENV === 'production') {
+    dbURI = process.env.MONGOLAB_URI;
+}
+
+const connect = () => {
+    setTimeout(() => mongoose.connect(dbURI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    }), 1000);
+}
+
+// CONNECTION EVENTS
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
+
+// CAPTURE APP TERMINATION / RESTART EVENTS
+// To be called when process is restarted or terminated
+gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+// For nodemon restarts
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+// For app termination
+process.on('SIGINT', function() {
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
+});
+// For Heroku app termination
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app termination', function() {
+        process.exit(0);
+    });
+});
+
+connect();
+
+// BRING IN YOUR SCHEMAS & MODELS
+require('./models/travlr');
